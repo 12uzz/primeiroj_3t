97112d2f52b56ff9ace61c712416d0626a02c586
12uzz <110130319+12uzz@users.noreply.github.com>
2022-10-11 15:47:17 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -41,3 +41,17 @@ function total(){
    let resultado = (val * (1+ (ju/100)));
    document.write("O resultado é " + resultado);
 }
+// Divisão é /
+// Multiplicação é *
+function soma(){
+   let v1 = document.getElementById("v1").value;
+   let v1 = document.getElementById("v2").value;
+   let r = (Number(v1) + Number(v2));
+   document.getElementById("resultado"). innerHTML = r;
+}
+function sub(){
+   let v1 = document.getElementById("v1").value;
+   let v1 = document.getElementById("v2").value;
+   let r = (Number(v1) + Number(v2));
+   document.getElementById("resultado"). innerHTML = r;
+}
